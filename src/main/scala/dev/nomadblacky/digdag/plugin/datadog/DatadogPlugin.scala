9606c037d39bc5c872438b3e6d7d4489bc41e806
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2020-03-12 01:35:41 +0900

    Tweaks
    Tested-by: NomadBlacky <tkadowaki.dev.blackey@gmail.com>

--- src/main/scala/dev/nomadblacky/digdag/plugin/datadog/DatadogPlugin.scala
+++ src/main/scala/dev/nomadblacky/digdag/plugin/datadog/DatadogPlugin.scala
@@ -8,9 +8,9 @@ import javax.inject.Inject
 
 class DatadogPlugin extends Plugin {
   override def getServiceProvider[T](`type`: Class[T]): Class[_ <: T] =
-    if (`type` eq classOf[OperatorProvider]) classOf[DatadogOperatorProvider] asSubclass (`type`) else null
+    if (`type` eq classOf[OperatorProvider]) classOf[DatadogOperatorProvider].asSubclass(`type`) else null
 }
 
-class DatadogOperatorProvider @Inject() (templateEngine: TemplateEngine) extends OperatorProvider {
+class DatadogOperatorProvider @Inject() extends OperatorProvider {
   override def get(): util.List[OperatorFactory] = util.Arrays.asList(new DatadogEventOperatorFactory())
 }
